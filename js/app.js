0d84c277b7e593078e51956b7b3d7ec2b44abcb6
mubadara <renaud.bineau@gmail.com>
2020-05-30 16:45:17 +0200

    third commit, css correction & quotes add

--- js/app.js
+++ js/app.js
@@ -10,6 +10,15 @@ let quotesArr = [
     "People who are doing things for fun do things the right way by themselves.",
     "When you say 'I wrote a program that crashed Windows,' people just stare at you blankly and say 'Hey, I got those with the system, for free.'",
     "Software is like sex: It's better when it's free.",
+    "Bill Gates really seems to be much more of a business man than a technologist, while I prefer to think of Linux in technical terms rather than as a means to money.",
+    "One of the reasons I like open source is that it allows people to work on the parts they are good at, and I don't mean just on a technical level; some people are into the whole selling and support, and that's just not me.",
+    "The thing with Linux is that the developers themselves are actually customers too: that has always been an important part of Linux.",
+    "That's what makes Linux so good: you put in something, and that effort multiplies. It's a positive feedback cycle.",
+    "Given enough eyeballs, all bugs are shallow.",
+    "I'm generally a very pragmatic person: that which works, works.",
+    "My name is Linus, and I am your God.",
+    "This 'users are idiots, and are confused by functionality' mentality of Gnome is a disease. If you think your users are idiots, only idiots will use it.",
+
 ];
 ///Change button & body color
 function changeEverything() {
